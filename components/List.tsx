15b8fb3698e2a3857b3ca8106c36afe9f28443b9
sapirost <sapir@xperiti.com>
2024-05-19 21:27:52 +0300

    convert app router to pages router

--- components/List.tsx
+++ components/List.tsx
@@ -5,7 +5,7 @@ import Item from './Item';
 const List = () => {
   return (
     <Box sx={{ flexGrow: 1, maxWidth: '100%' }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{ maxWidth: '100%' }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{ maxWidth: '100%', margin: 0 }}>
         {Array.from(Array(6)).map((_, index) => (
           <Grid xs={2} sm={4} md={4} key={index}>
             <Item>xs=2</Item>
