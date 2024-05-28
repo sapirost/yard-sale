78d8930847670f6dbe78125bca6f7041049ce992
sapirost <sapir@xperiti.com>
2024-05-28 20:49:24 +0300

    create storage context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { StorageProvider } from '@/contexts/storageContext';
+import '@/styles/globals.scss';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+  <StorageProvider>
+    <Component {...pageProps} />
+  </StorageProvider>
+);
 
 export default App;
